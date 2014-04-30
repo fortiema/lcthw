8538e291e4de0f5de00db82ad624c35d3956fb87
Matt Fortier <fortiema@gmail.com>
2014-04-30 09:24:23 -0400

    Added more tests and verbrosity to ex17 script.

--- ex17/ex17-autotest.sh
+++ ex17/ex17-autotest.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# Matt Fortier
+# 04-2014
+
 DBFile="ex17-db.dat"
 
 function CheckDBExistence()
@@ -16,10 +19,15 @@ function CheckDBExistence()
     fi
 }
 
+# Creates the db file
 function TestCreateDB()
 {
     ./ex17 $DBFile c
-    CheckDBExistence
+    if [ -f $DBFile ]; then
+        echo "Create DB: SUCCESS!"
+    else
+        echo "Create DB: FAIL"
+    fi
 }
 
 function SetEntry()
@@ -33,11 +41,25 @@ function SetEntry()
     fi
 }
 
+function DeleteDBFile()
+{
+    CheckDBExistence
+    rm -f $DBFile
+    if [ ! -f $DBFile ]; then
+        echo "Delete DB: SUCCESS!"
+    else
+        echo "Delete DB: FAIL"
+    fi
+}
+
 echo "Starting auto-test suite for ex17..."
 
 TestCreateDB
+echo "Testing Data Entry..."
 SetEntry 1 Matt mfortier@jupiter.com
 SetEntry 2 Yan yroy@jupiter.com
+echo "Data Entry Mechanism tested successfully!"
+DeleteDBFile
 
 echo "All tests passed! Exiting..."
 
